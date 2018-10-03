197d2b0dda21784b8f847a54ea05679d6047aa98
Yuriy Poralo <mrbighokage@gmail.com>
2018-10-03 19:36:16 +0300

    Add Test

---
+++ Test/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading.Tasks;
+using ClientWebSocketWrapper;
+
+namespace Test
+{
+    class Program
+    {
+        public delegate Task Test();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Gdax");
+            Task.Delay(200).GetAwaiter().GetResult();
+
+            RunAsync(TestGdax).GetAwaiter().GetResult();
+
+            Console.WriteLine("Test Poloniex");
+            Task.Delay(200).GetAwaiter().GetResult();
+
+            RunAsync(TestPoloniex).GetAwaiter().GetResult();
+        }
+
+        private static async Task TestGdax()
+        {
+            var uri = new Uri("wss://ws-feed.gdax.com");
+
+            using (var socket = new WebSocketWrapper(uri))
+            {
+                await socket.ConnectAsync();
+
+                // check events
+                MessageArrived(socket);
+
+                await socket.SendAsync(new
+                {
+                    type = "subscribe",
+                    product_ids = new string[] { "ETH-USD", "ETH-EUR" }
+                });
+
+                await Task.Delay(10000);
+            }
+        }
+
+        private static async Task TestPoloniex() {
+            var uri = new Uri("wss://api2.poloniex.com");
+
+            using (var socket = new WebSocketWrapper(uri))
+            {
+                await socket.ConnectAsync();
+
+                // check events
+                MessageArrived(socket);
+
+                 // subscribe to chanell BTC_NXT
+                await socket.SendAsync(new
+                {
+                    command = "subscribe",
+                    channel = 69
+                });
+
+                await Task.Delay(10000);
+
+                await socket.SendAsync(new
+                {
+                    command = "unsubscribe",
+                    channel = 69
+                });
+
+                await Task.Delay(2000);
+
+                await socket.SendAsync(new
+                {
+                    command = "subscribe",
+                    channel = 7
+                });
+
+                await Task.Delay(10000);
+
+                await socket.SendAsync(new
+                {
+                    command = "unsubscribe",
+                    channel = 7
+                });
+            }
+        }
+
+        private static void MessageArrived(WebSocketWrapper socket) {
+            socket.MessageArrived += (message) =>
+            {
+                Console.WriteLine(message);
+            };
+
+            socket.ConnectionClosed += () =>
+            {
+                Console.WriteLine("Connection Closed");
+            };
+
+            socket.ConnectionError += (exception) =>
+            {
+                Console.WriteLine("Connection Error");
+            };
+        }
+
+        private static async Task RunAsync(Test test) {
+            try
+            {
+                do
+                {
+                    await test();
+
+                    Console.WriteLine("\nType \"exit\" to run next test or close application.");
+                }
+                while (!Console.ReadLine().ToLower().Equals("exit"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
